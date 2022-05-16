bface9358a50ce8fb492fcbf00935cab27e513dd
Marcos Eduardo Winiarski <marcos.winiarski@ambevtech.com.br>
2022-05-15 23:25:22 -0300

    feat: Criação de página de Favoritos

---
+++ src/pages/Favorites/styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { PageContainerStyle, PageTitleStyle } from '../../styles/pages'
+
+export const Favorites = styled.section`
+  ${PageContainerStyle}
+`
+export const Title = styled.h1`
+  ${PageTitleStyle}
+`
