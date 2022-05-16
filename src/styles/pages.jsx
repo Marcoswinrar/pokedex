9644f500b891d984b750109bdaa50893a1460e7d
Marcos Eduardo Winiarski <marcos.winiarski@ambevtech.com.br>
2022-05-15 23:26:16 -0300

    feat: Criação de style global para paginas

---
+++ src/styles/pages.jsx
@@ -0,0 +1,17 @@
+import { css } from 'styled-components'
+
+const PageContainerStyle = css`
+  width: 90%;
+  margin: 2rem auto;
+`
+const PageTitleStyle = css`
+  color: crimson;
+  font-weight: lighter;
+  background-color: white;
+  padding: 12px;
+  border-left: 15px solid crimson;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+`
+
+export { PageContainerStyle, PageTitleStyle }
