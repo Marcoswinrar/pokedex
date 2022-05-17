3db0914a85ecd662f1f4911fe29dd30262968e19
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-17 01:34:03 -0300

    Criação de cores e cores por tipo para pokedex

--- src/components/Header/styled.jsx
+++ src/components/Header/styled.jsx
@@ -1,17 +1,14 @@
 import styled from 'styled-components'
-// import media from 'styled-media-query'
+import Colors from '../../styles/colors'
 
 export const Header = styled.header`
-  height: 95px;
-  background-color: #c0392b;
+  height: 50px;
+  background-color: ${Colors.redDefault};
 `
 
 export const Title = styled.h1`
   margin: 0;
   color: white;
   font-weight: lighter;
-  font-size: 3rem;
   text-align: center;
-  vertical-align: middle;
-  padding: 12px;
 `
---
+++ src/styles/colors.jsx
@@ -0,0 +1,5 @@
+const Colors = {
+  redDefault: '#c0392b'
+}
+
+export default Colors
