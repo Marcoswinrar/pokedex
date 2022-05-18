4840a77b5d4f277380911059f865c318bdbc8d2d
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-18 01:07:22 -0300

    Criação de componente para listar os tipos de cada Pokemon

---
+++ src/components/PokemonTypes/styled.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+import Colours from '../../styles/pokemonTypes'
+
+export const Types = styled.div`
+  padding: 12px;
+  display: flex;
+  justify-content: center;
+`
+
+export const Type = styled.span`
+  ${(props) => props.width && 'width: 42px'};
+  background-color: ${(props) => Colours[props.color]};
+  color: white;
+  margin: 3px;
+  padding: 5px;
+  text-align: center;
+  text-transform: capitalize;
+  border-radius: 3px;
+`
