2720da80a23eee8da6557b55a7dd91b01868bc89
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-18 23:55:01 -0300

    Remoção do navbar do layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,10 @@
 import PropTypes from 'prop-types'
 import Header from '../Header'
-import Navbar from '../Navbar'
 import LayoutContainer from './styled'
 
 const Layout = ({ children }) => (
   <LayoutContainer>
     <Header />
-    <Navbar />
     {children}
   </LayoutContainer>
 )
