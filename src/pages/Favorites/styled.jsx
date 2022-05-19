37a174e93a2810e40f4052a463163bd007a54c12
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-18 23:54:22 -0300

    Arquivos deletados

--- src/pages/Favorites/styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { PageContainerStyle, PageTitleStyle } from '../../styles/pages'
-
-export const Favorites = styled.section`
-  ${PageContainerStyle}
-`
-export const Title = styled.h1`
-  ${PageTitleStyle}
-`
--- src/pages/Home/styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { PageContainerStyle } from '../../styles/pages'
-
-export const Home = styled.section`
-  ${PageContainerStyle}
-`
-
-export default Home
