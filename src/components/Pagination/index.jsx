9f7666e4d21985feaec154fec134b265613cc2a1
Marcoswinrar <marcoswinrar@gmail.com>
2022-05-19 23:31:14 -0300

    Refatoração para melhora do componente de paginação

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,30 +1,26 @@
 import PropTypes from 'prop-types'
 import * as S from './styled'
 
-const Pagination = ({ pagination, onClick }) => {
-  const paginate = (page) => onClick(page)
-
-  return (
-    <S.Paginator>
-      <S.Button
-        disabled={!pagination.previous}
-        onClick={() => paginate(pagination.previous)}
-      >
-        Anterior
-      </S.Button>
-      <S.Button
-        disabled={!pagination.next}
-        onClick={() => paginate(pagination.next)}
-      >
-        Próxima
-      </S.Button>
-    </S.Paginator>
-  )
-}
+const Pagination = ({ pagination, onPaginate }) => (
+  <S.Paginator>
+    <S.Button
+      disabled={!pagination.previous}
+      onClick={() => onPaginate(pagination.previous)}
+    >
+      Anterior
+    </S.Button>
+    <S.Button
+      disabled={!pagination.next}
+      onClick={() => onPaginate(pagination.next)}
+    >
+      Próxima
+    </S.Button>
+  </S.Paginator>
+)
 
 Pagination.propTypes = {
   pagination: PropTypes.objectOf(PropTypes.string).isRequired,
-  onClick: PropTypes.func.isRequired
+  onPaginate: PropTypes.func.isRequired
 }
 
 export default Pagination
